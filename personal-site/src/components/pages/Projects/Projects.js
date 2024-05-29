e0a703af39e8a092ec4f4d8a7fc29d947e24d672
John Stevens <johnstevens2424@gmail.com>
2024-05-29 12:44:33 -0600

    Got some work done on the projects page

--- personal-site/src/components/pages/Projects/Projects.js
+++ personal-site/src/components/pages/Projects/Projects.js
@@ -1,10 +1,34 @@
 import { Outlet, Link } from "react-router-dom";
-
+import './ProjectsStyles.css'
 
 const Projects = () => {
+
+    let projects = [
+        {name:"TILT", type:"Mobile App", techStack:"React Native", description:"something", githubLink:"https://github.com/johnstevens24/Tilt"},
+        {name:"TellMe", type:"Mobile App", techStack:"React Native", description:"capstone project blah blah", githubLink:"https://github.com/johnstevens24/TellMe"},
+        {name:"SLC Air Quality", type:"Data Visualization", techStack:"Python (pandas library)", description:"capstone project blah blah", githubLink:"https://github.com/johnstevens24/MobileAirQuality"},
+        {name:"SCI Research", type:"Research Paper", techStack:"None", description:"investigation into...", githubLink:""}
+
+    ]
+
+
+
+
+
     return(
         <>
-        <text>projects</text>
+        <div>
+            {projects.map((project) => (
+                <div className="projectDiv">
+                    <h2 className={project.name}>{project.name}</h2>
+                    <p><b>Type:</b> {project.type}</p>
+                    <p><b>Tech Stack:</b> {project.techStack}</p>
+                    <p>{project.description}</p>
+                    <div style={{flex:1}}/>
+                    <a href={project.githubLink}>GitHub Link</a>
+                </div>
+            ))}
+        </div>
         </>
     )
 }
