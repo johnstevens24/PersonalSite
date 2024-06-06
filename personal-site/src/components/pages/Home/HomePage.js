7a329db388acfe2bc0f725ed431935c27c046cef
John Stevens <johnstevens2424@gmail.com>
2024-06-06 16:18:48 -0600

    Done with bio and languages containers (mostly)

--- personal-site/src/components/pages/Home/HomePage.js
+++ personal-site/src/components/pages/Home/HomePage.js
@@ -3,21 +3,20 @@ import React from 'react'
 import "./HomePageStyles.css"
 
 const HomePage = () => {
-
+    const xLight = "#f2f2f2"
     const languages = [
         {name:"JavaScript", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"]},
         {name:"Python", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"]},
         {name:"Java", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"]},
-        {name:"HTML5/CSS", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg","https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"]},
+        {name:"HTML5/CSS", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"]},
         {name:"SQL", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg"]},
         {name:"sqlite", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg"]},
-        {name:"C/C++/C#", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg"]},
+        {name:"C/C++/C#", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg"]},
         {name:"JSON", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/json/json-original.svg"]},
-        {name:"XML", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/xml/xml-original.svg"]},
         {name:"React", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"]},
-        {name:"Github", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"]}, 
-        {name:"Gitlab", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original.svg"]}, 
-        {name:"Django", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/djangorest/djangorest-original.svg"]},
+        // {name:"Github", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"]}, 
+        // {name:"Gitlab", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original.svg"]}, 
+        // {name:"Django", icon:["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/djangorest/djangorest-original.svg"]},
         ]
     
     const bioStuff = (
@@ -46,18 +45,31 @@ const HomePage = () => {
                 <hr></hr>
                 {/* Languages container */}
                 <div className='languagesContainer'>
-                    <div style={{width:'40%', marginRight:'10%'}}>
-                        <h2>Tech stack I'm familiar with</h2>
-                        <p>Languages and libraries that I've used for different projects and have experience with.</p>
+                    <div style={{width:'40%'}}>
+                        <h2>I'm most familiar with these languages</h2>
+                        <p>I have used languages and technologies, but these ones are where most of my experience lies.</p>
                     </div>
-                    <div style={{width:'50%'}}>
-                        {languages.map(() => (
-                            <text>hi</text>
+                    <div className='languageGrid'>
+                        {languages.map((language, index) => (
+                            <div key={index} style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'space-between', height:'70px', width:'100%', backgroundColor:xLight, borderRadius:10, padding:'10px', paddingRight:'15%', paddingLeft:'15%'}}>
+                                <div style={{position:'relative', height:'50px', aspectRatio:1}}>
+                                    {language.icon.length > 1 ? 
+                                        language.icon.map((icon, index) => (
+                                            
+                                            <img src={icon} style={{height:'50px', aspectRatio:1, position:'absolute', top:0, left:(language.icon.length-(1+index))*25}}/>
+                                            
+                                        ))
+                                    :
+                                        <img src={language.icon} style={{height:'50px', aspectRatio:1}}/>
+                                    }
+                                </div>
+                                <text>{language.name}</text>
+                            </div>
                         ))}
                     </div>
 
                 </div>
-
+                <hr></hr>
                 {/* Projects container */}
                 <div>
 
